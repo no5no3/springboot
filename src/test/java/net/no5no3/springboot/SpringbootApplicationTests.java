f955e7069ac266e3fd6c73d09eb153f0f5867a8c
hirenloong <hirenloong@gmail.com>
2018-03-04 13:06:14 +0800

    test pass

--- src/test/java/net/no5no3/springboot/SpringbootApplicationTests.java
+++ src/test/java/net/no5no3/springboot/SpringbootApplicationTests.java
@@ -5,17 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.ResponseExtractor;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.byLessThan;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,9 +17,9 @@ public class SpringbootApplicationTests {
 	ApplicationContext ctx;
 	@Test
 	public void contextLoads() {
-
-		RestTemplate restTemplate = new RestTemplate();
-		System.out.println(restTemplate.getForObject("http://localhost:8081/h/vo/0?size=20",String.class));
+		assertThat((VocabularyController)ctx.getBean("vocabularyController"));
+//		RestTemplate restTemplate = new RestTemplate();
+//		System.out.println(restTemplate.getForObject("http://localhost:8081/h/vo/0?size=20",String.class));
 //		restTemplate.execute(, HttpMethod.GET, request -> System.out.println(request.toString()), new ResponseExtractor<String>() {
 //			@Override
 //			public String extractData(ClientHttpResponse response) throws IOException {
